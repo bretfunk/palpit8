ecf17555d13cdc0006dc4383ee66e695eefbfa5f
MikelSage <michael.sagapolutele@yahoo.com>
2017-08-31 15:20:01 -0600

    add auto scrolling to chat

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
   get "/dashboard", to: "dashboard#show"
 
+  resources :messages
+  resources :chatrooms, param: :slug
+
+  mount ActionCable.server => '/cable'
 end

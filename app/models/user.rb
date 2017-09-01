72b1dbba6c36ff09aabe688180a67632506d62e4
MasonHolland <masontholland@gmail.com>
2017-08-31 20:33:03 -0600

    add relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
+
+  has_one :role
 
   def self.find_or_create_by_auth(auth)
     user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid'])

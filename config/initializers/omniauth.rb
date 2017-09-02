767db3703fc17e9274158375c7ee52b83744ee96
BFunk <bret.e.funk@gmail.com>
2017-09-02 17:00:14 -0600

    updated oauth with scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitch, ENV["TWITCH_CLIENT_ID"], ENV["TWITCH_CLIENT_SECRET"], scope: [channel_editor, channel_feed_edit, channel_feed_read, chat_login, user_follows_edit, user_read, user_subscriptions]
+  provider :twitch, ENV["TWITCH_CLIENT_ID"], ENV["TWITCH_CLIENT_SECRET"], scope: 'channel_editor, channel_feed_edit, channel_feed_read, chat_login, user_follows_edit, user_read, user_subscriptions'
 end

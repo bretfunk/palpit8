26373671f447ea76757c9ac484bb35480918732f
JF-Lalonde <lalondej88@gmail.com>
2017-09-02 13:01:36 -0600

    Update removal of AJAX spike content from home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,10 +6,4 @@ class HomeController < ApplicationController
   def new
   end
 
-  def refresh_part
-    @data = WatsonService.new.watson_tone('log/twitch_chat.log')
-    respond_to do |format|
-      format.js
-    end
-  end
 end

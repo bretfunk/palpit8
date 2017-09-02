5f50d57bd7065ffa44f7de225b8794b15c5a828e
MasonHolland <masontholland@gmail.com>
2017-09-02 16:19:41 -0600

    add current_users_profile_info method to twitch service

--- app/services/twitch_service.rb
+++ app/services/twitch_service.rb
@@ -3,7 +3,12 @@ class TwitchService
     @conn = Faraday.new("https://api.twitch.tv")
   end
 
-  def current_users_profile
-    url = "/kraken/user"
+  def current_users_profile_info
+    response = @conn.get do |req|
+      req.url                      = "/kraken/user"
+      req.headers['Client-ID']     = ENV['TWITCH_CLIENT_ID']
+      req.headers['Authorization'] = "OAuth #{current_user.token}"
+    end
+    user_info = JSON.parse(response.body, symbolize_names: true)
   end
 end

f85e4d94128b5a2ee9b1f9537ec6df685bfb6646
BFunk <bret.e.funk@gmail.com>
2017-09-04 15:43:18 -0600

    Mikes code to make functional chatrooms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_by_auth(request.env['omniauth.auth'])
     if @user
+      cookies.signed[:user_id] = @user.id
       session[:user_id] = @user.id
       redirect_to dashboard_path
     else

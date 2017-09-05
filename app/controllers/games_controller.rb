949e5b4deebb4d6f125fc80ab325361748b8bd8a
BFunk <bret.e.funk@gmail.com>
2017-09-05 15:48:30 -0600

    adding more model tests and removing game in liu of chatrooms

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,12 @@
 class GamesController < ApplicationController
 
   def index
-    @games = Game.all
+    #@games = Game.all
+    @chatrooms = Chatroom.all
   end
 
   def show
-    @game = Game.find_by(slug: params[:slug])
-    #@chatroom = Chatroom.find_by(slug: @game.name.parameterize)
+    #@game = Game.find_by(slug: params[:slug])
     @chatroom = Chatroom.find_by(slug: params[:slug])
     @message = Message.new
   end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
   context "validations" do
-    #it { should validate_presence_of(:name) }
+    it "is valid with valid attributes" do
+    game = Game.create(name: "Starcraft 2")
+    expect(game.name).to eq("Starcraft 2")
+    expect(game.slug).to eq("starcraft-2")
+    end
   end
 end

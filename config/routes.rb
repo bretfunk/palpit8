627ac9a038950bc46c29239273454a2cc8690c77
MasonHolland <masontholland@gmail.com>
2017-09-07 17:34:35 -0600

    adjust 'home' page to games index. remove home controller and views. update navbar login and louout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "home#index"
+  root "games#index"
   get "/auth/:provider/callback", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
   get "/dashboard", to: "dashboard#show"

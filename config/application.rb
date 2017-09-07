0a58c13039ea7248d7d89e91219ede880e939037
Michael Centrelli <Centm860@newschool.edu>
2017-09-07 17:51:15 -0600

    Support for Iframe fixed

--- config/application.rb
+++ config/application.rb
@@ -16,14 +16,14 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.action_dispatch.default_headers = {
-  'X-Frame-Options' => 'ALLOWALL'
-}
 
 module TwitchPractice
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.action_dispatch.default_headers = {
+          'X-Frame-Options' => 'ALLOWALL'
+          }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

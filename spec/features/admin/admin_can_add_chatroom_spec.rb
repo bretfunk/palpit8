e04ad09193f166a75679a3cf343d0206b0ae8c5d
BFunk <bret.e.funk@gmail.com>
2017-09-07 20:05:02 -0600

    updating admin tests

--- spec/features/admin/admin_can_add_chatroom_spec.rb
+++ spec/features/admin/admin_can_add_chatroom_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
-feature "Admin can see games" do
-  scenario "Admin can see all games (chatrooms)" do
+feature "Admin can see games(chatrooms)" do
+  scenario "Admin can see all games(chatrooms)" do
     admin = create(:user)
+    admin_role = create(:role, name: "admin")
+    admin.roles << admin_role
     game1 = create(:chatroom, topic: "game1")
     game2 = create(:chatroom, topic: "game2")
 
@@ -12,9 +14,9 @@ feature "Admin can see games" do
     visit admin_chatrooms_path
     expect(page).to have_content(game1.topic)
     expect(page).to have_content(game2.topic)
-    expect(page).to have_css("chatroom", count: 2)
+    expect(page).to have_css(".chatroom", count: 2)
 
-    click_on "Add Chatroom"
+    click_on "New Chatroom"
     expect(current_path).to eq(new_admin_chatroom_path)
     fill_in("chatroom_topic", with: "Candy Crush")
     click_on "Create Chatroom"
--- spec/features/admin/admin_can_edit_chatrooms_spec.rb
+++ spec/features/admin/admin_can_edit_chatrooms_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
-feature "Admin can see games" do
-  scenario "Admin can see all games (chatrooms)" do
+feature "Admin can edit games(chatrooms)" do
+  scenario "Admin can edit games(chatrooms)" do
     admin = create(:user)
+    admin_role = create(:role, name: "admin")
+    admin.roles << admin_role
     game1 = create(:chatroom, topic: "game1")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user)
@@ -10,9 +12,8 @@ feature "Admin can see games" do
 
     visit admin_chatrooms_path
     expect(page).to have_content(game1.topic)
-    expect(page).to_css("chatroom", count: 1)
+    expect(page).to have_css(".chatroom", count: 1)
     click_on("Edit")
-    expect(current_path).to eq(edit_admin_chatroom_path)
     fill_in("chatroom_topic", with: "Candy Crush")
     click_on("Update Chatroom")
     expect(current_path).to eq(admin_chatrooms_path)
